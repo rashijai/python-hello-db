3b80060bb13ba83184e3a4f1b7f0c492cd521074
rashijai <75713664+rashijai@users.noreply.github.com>
2024-01-31 20:47:40 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
-import telnetlib 
-  
-tn = telnetlib.Telnet("database-1-instance-1.c6ralslvhwx7.us-east-1.rds.amazonaws.com", 3306) 
-output = tn.read_all() 
-print (output)
-tn.close()
+import os
+import subprocess
 
+test_cmd = ' nc -vz database-1-instance-1.c6ralslvhwx7.us-east-1.rds.amazonaws.com 3306'
+
+process = subprocess.Popen(test_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+err = process.stderr.read()
+print str(err)
+ans = process.stdout.read()
+
+print str(ans)
