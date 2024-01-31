fa54de835ae8dd2e8a13e878575442942528e371
rashijai <75713664+rashijai@users.noreply.github.com>
2024-01-31 20:32:18 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import telnetlib 
   
 tn = telnetlib.Telnet("database-1-instance-1.c6ralslvhwx7.us-east-1.rds.amazonaws.com", 3306) 
-print (tn)
+output = tn.read_all() 
+print (output)
+tn.close()
 
