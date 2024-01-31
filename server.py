d3457ca52094d48ca77e7b3f2812ef3b9f0302f0
rashijai <75713664+rashijai@users.noreply.github.com>
2024-01-31 21:03:35 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,9 @@
 import os
 import subprocess
-
 test_cmd = 'nc -vz database-1-instance-1.c6ralslvhwx7.us-east-1.rds.amazonaws.com 3306'
-
+# process = subprocess.Popen(test_cmd, shell=True)
 process = subprocess.Popen(test_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 err = process.stderr.read()
-print str(err)
+print(str(err))
 ans = process.stdout.read()
-
-print str(ans)
+print(str(ans))
